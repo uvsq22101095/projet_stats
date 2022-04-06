543814b193b10b5f5c11f4094b3a5f77a8099bc2
johan <johan.nimeth@gmail.com>
2022-04-06 09:31:05 +0200

    test

---
+++ projet_stats.py
@@ -0,0 +1,9 @@
+#########################################
+# groupe MIASHS 1
+# Nathan TIPAKA
+# Johan CHENG
+# Clara SAUNIER (SION-SAUNIER)
+# Michele MARQUES LEBEN
+# https://github.com/uvsq22101095/projet_stats
+#########################################
+
