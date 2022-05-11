d650e995c10339991dee87c1dd6e00c4caa4f47d
uvsq22107022 <91381233+uvsq22107022@users.noreply.github.com>
2022-05-11 11:18:11 +0200

    Update projet_stats.py

--- projet_stats.py
+++ projet_stats.py
@@ -3,7 +3,7 @@
 # Nathan TIPAKA
 # Johan CHENG
 # Clara SAUNIER (SION-SAUNIER)
-# https://github.com/uvsq22101095/projet_statss
+# https://github.com/uvsq22101095/projet_stats
 #########################################
 
 import random as rd
